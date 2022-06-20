071831f893b268dd6f6b0f432589619963fcf902
andreluiz-grafeno <andre.luiz@grafeno.digital>
2022-06-20 16:38:52 -0300

    Validando notificação

--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -4,6 +4,9 @@ describe 'Preenchimento de Formulário simples' do
         fill_in 'userId', with: 'stark'
         fill_in 'passId', with: 'jarvis!'
         click_button 'Login'
-        find('#flash').visible?.should == true
+
+        expect(find('#flash').visible?).to be true
+
+        expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
     end
 end
